92961e2fd706cc0911de1b4bfa2db86638966537
KirillMysnik <kirill@mysnik.com>
2017-04-25 13:15:22 +0300

    The plugin now sets sv_airaccelerate and sv_enablebunnyhopping by itself - useful if they're protected New-style info

--- addons/source-python/plugins/infinite_jumping/infinite_jumping.py
+++ addons/source-python/plugins/infinite_jumping/infinite_jumping.py
@@ -1,3 +1,8 @@
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python
+from cvars import ConVar
 from entities.constants import MoveType
 from entities.hooks import EntityCondition, EntityPreHook
 from memory import make_object
@@ -6,9 +11,21 @@ from players.constants import PlayerButtons, PlayerStates
 from players.entity import Player
 
 
-from .info import info
+# =============================================================================
+# >> GLOBAL VARIABLES
+# =============================================================================
+AIR_ACCELERATE = 9999
+
+cvar_enablebunnyhopping = ConVar("sv_enablebunnyhopping")
+cvar_airaccelerate = ConVar("sv_airaccelerate")
+
+old_cvar_value_enablebunnyhopping = None
+old_cvar_value_airaccelerate = None
 
 
+# =============================================================================
+# >> HOOKS
+# =============================================================================
 @EntityPreHook(EntityCondition.is_human_player, 'run_command')
 def pre_run_command(args):
     player = make_object(Player, args[0])
@@ -31,3 +48,20 @@ def pre_run_command(args):
 
     user_cmd = make_object(UserCmd, args[1])
     user_cmd.buttons &= ~PlayerButtons.JUMP
+
+
+# =============================================================================
+# >> LOAD & UNLOAD FUNCTIONS
+# =============================================================================
+def load():
+    global old_cvar_value_enablebunnyhopping, old_cvar_value_airaccelerate
+    old_cvar_value_enablebunnyhopping = cvar_enablebunnyhopping.get_bool()
+    old_cvar_value_airaccelerate = cvar_airaccelerate.get_int()
+
+    cvar_enablebunnyhopping.set_bool(True)
+    cvar_airaccelerate.set_int(AIR_ACCELERATE)
+
+
+def unload():
+    cvar_enablebunnyhopping.set_bool(old_cvar_value_enablebunnyhopping)
+    cvar_airaccelerate.set_int(old_cvar_value_airaccelerate)
